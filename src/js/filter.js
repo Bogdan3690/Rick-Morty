49224a3a63e45bcd03ce408f8f4f937f5a3ab2d2
Bogdan3690 <dblevko@gmail.com>
2026-07-08 13:11:07 -0600

    add filters

--- src/js/filter.js
+++ src/js/filter.js
@@ -1,29 +1,46 @@
-import { createCards } from "./create-markup"
-import { fetchCharacters } from "./server"
+import { createCards } from './create-markup';
+import { fetchCharacters } from './server';
 
-const input = document.querySelector(".name")
-const btn = document.querySelector(".filter-name-btn")
-const status = document.querySelector("#status")
-const species = document.querySelector("#species")
-const gender = document.querySelector("#gender")
-const list = document.querySelector(".character-list")
-console.log(input);
+const input = document.querySelector('.name');
+const btn = document.querySelector('.filter-name-btn');
+const btnLoadMore = document.querySelector('.js-load-more');
+const status = document.querySelector('#status');
+const species = document.querySelector('#species');
+const gender = document.querySelector('#gender');
+const list = document.querySelector('.character-list');
+const errorImg = document.querySelector('.error-img');
+console.log(errorImg);
 
 // input.addEventListener("input", onInput)
-btn.addEventListener("click", onInput)
+btn.addEventListener('click', onInput);
+status.addEventListener('change', onInput);
+species.addEventListener('change', onInput);
+gender.addEventListener('change', onInput);
 
 async function onInput(e) {
+  const value = input.value;
+  console.log(status.value);
 
-            const value = input.value
-    console.log(value);
-    try {
+  const statusV = status.value;
+  const speciesV = species.value;
+  const genderV = gender.value;
+  console.log(value, statusV, speciesV, genderV);
+  try {
+    const data = await fetchCharacters(1, value, statusV, speciesV, genderV);
+    list.innerHTML = '';
+    console.log(data);
+    console.log(1, data.info.pages);
 
-    const data = await fetchCharacters(1, value)
-    list.innerHTML= ""
-    createCards(data.results)
-    } catch (error) {
-        console.log(error);
-        
+    errorImg.style.display = 'none';
+    btnLoadMore.style.display = 'block';
+    if (data.info.pages < 2) {
+      btnLoadMore.style.display = 'none';
     }
-}
 
+    createCards(data.results);
+  } catch (error) {
+    console.log(error);
+    errorImg.style.display = 'block';
+    btnLoadMore.style.display = 'none';
+  }
+}
--- src/js/load-more.js
+++ src/js/load-more.js
@@ -1,32 +1,45 @@
-import { createCards } from "./create-markup";
-import { fetchCharacters } from "./server";
+import { createCards } from './create-markup';
+import { fetchCharacters } from './server';
 
-const btn = document.querySelector(".js-load-more")
-// const list = document.querySelector(".character-list")
-let countOfPages = 1
-let page = 1
+const btn = document.querySelector('.js-load-more');
+const list = document.querySelector('.character-list');
+const input = document.querySelector('.name');
+const status = document.querySelector('#status');
+const species = document.querySelector('#species');
+const gender = document.querySelector('#gender');
+let countOfPages = 1;
+let page = 1;
 
-btn.addEventListener("click", onLoadMoreBtnClick)
+btn.addEventListener('click', onLoadMoreBtnClick);
 
 async function onLoadMoreBtnClick(params) {
-   try {
-   page += 1
+  try {
+    page += 1;
 
-        btn.disabled = true
-   const data = await fetchCharacters(page)
-   console.log(data.info.pages);
-   countOfPages = data.info.pages
+    btn.disabled = true;
+    const statusV = status.value;
+    const speciesV = species.value;
+    const genderV = gender.value;
+    const data = await fetchCharacters(
+      page,
+      input.value,
+      statusV,
+      speciesV,
+      genderV
+    );
 
-       if (page > countOfPages) {
-        console.log(page > countOfPages);
-        btn.style.display = "none"
-        return
+    console.log(data.info.pages);
+    countOfPages = data.info.pages;
+    console.log(page, countOfPages);
+
+    createCards(data.results);
+    if (page >= countOfPages) {
+      console.log(page >= countOfPages);
+      btn.style.display = 'none';
+      return;
     }
-    btn.disabled = false
-    createCards(data.results)
-   } catch (error) {
+    btn.disabled = false;
+  } catch (error) {
     console.log(error);
-
-    
-   }
+  }
 }
